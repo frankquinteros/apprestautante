b17174a17a9b7e5e67da41c681a32ee046df559d
marlene salazar <marlenesalazar1995@gmail.com>
2018-12-11 14:57:32 -0400

    registro de restaurantes

---
+++ app/src/main/java/com/example/frank/proyectores/utils/Data.java
@@ -0,0 +1,7 @@
+package com.example.frank.proyectores.utils;
+
+public class Data  {
+public static String TOKEN =  " Data eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiY2FybG9zMTk5NUBnbWFpbC5jb20iLCJwYXNzd29yZCI6Ijg5MCIsImlhdCI6MTU0NDQ5NjQzMX0.RXxGyss24RCYJuIzczfQKSEmUlwsVTqP40_xa69mEbI";
+public static String HOST = "http://172.19.0.1:7777/api/v1.0";
+public static String REGISTER_RESTORANT = HOST + "/RESTAURANT";
+}
